9946710c6c0b1e8e1cb6288ac2db476f37e28af1
Joao Ramos <joao.f.ramos@tecnico.ulisboa.pt>
2018-04-18 16:15:36 +0100

    ports changed

--- src/init.cpp
+++ src/init.cpp
@@ -232,7 +232,7 @@ std::string HelpMessage()
         "  -socks=<n>             " + _("Select the version of socks proxy to use (4-5, default: 5)") + "\n" +
         "  -tor=<ip:port>         " + _("Use proxy to reach tor hidden services (default: same as -proxy)") + "\n"
         "  -dns                   " + _("Allow DNS lookups for -addnode, -seednode and -connect") + "\n" +
-        "  -port=<port>           " + _("Listen for connections on <port> (default: 9030 or testnet: 19030)") + "\n" +
+        "  -port=<port>           " + _("Listen for connections on <port> (default: 16334 or testnet: 16334)") + "\n" +
         "  -maxconnections=<n>    " + _("Maintain at most <n> connections to peers (default: 125)") + "\n" +
         "  -addnode=<ip>          " + _("Add a node to connect to and attempt to keep the connection open") + "\n" +
         "  -connect=<ip>          " + _("Connect only to the specified node(s)") + "\n" +
@@ -633,7 +633,7 @@ bool AppInit2()
         FILE* file = fopen((GetDataDir() / "blockstat.dat").string().c_str(), "w");
         if (!file)
            return false;
-        
+
         for (map<uint256, CBlockIndex*>::iterator mi = mapBlockIndex.begin(); mi != mapBlockIndex.end(); ++mi)
         {
             CBlockIndex* pindex = (*mi).second;
@@ -798,4 +798,3 @@ bool AppInit2()
 
     return true;
 }
-
